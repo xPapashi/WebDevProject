d02cefaed69a6e7f639d89b5820f7fefb1db5595
xPapashi <dtomkowski1@gmail.com>
2023-02-01 18:49:23 +0000

    Add side menu with options

---
+++ app.js
@@ -0,0 +1,28 @@
+const sidebar = document.querySelector('#mySideNav');
+const sidebarIcon = document.querySelector('#sidebar-icon');
+const body = document.querySelector('body');
+
+function openNav() {
+    sidebar.style.width = '250px';
+    sidebar.style.padding = '16px';
+    sidebar.style.borderRight = '3px solid hsla(0, 0%, 42%, 0.4)';
+    sidebarIcon.style.display = 'none';
+
+}
+
+function closeNav() {
+    body.addEventListener('click', function (e) {
+        console.log(e);
+        if (!(e.target.id === "mySideNav" || 
+        e.target.className === "sidebar-content" ||
+        e.target.className === "sidebar-title" ||
+        e.target.localName === "ul" ||
+        e.target.localName === "li" ||
+        e.target.localName === "a")) {
+            sidebarIcon.style.display = 'block';
+            sidebar.style.width = '0px';
+            sidebar.style.padding = '0px';
+            sidebar.style.borderRight = '0';
+        };
+    }, true);
+}
