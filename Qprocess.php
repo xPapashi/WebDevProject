db24f53bbe000f33cbe7ae6fde17409437d7ac6a
Semas2001 <semasarmonaitis@gmail.com>
2023-02-26 23:21:20 +0000

    added more to the next function

--- Qprocess.php
+++ Qprocess.php
@@ -26,8 +26,27 @@
   if ($_POST){
     $number = $_POST['number'];
     $selected_choice = $_POST['choice'];
-    $next = $number+1;
+    $next = $number++;
 
+
+    
     $query = "SELECT * FROM `CHOICE` WHERE question_number = $number AND is_correct = 1";
+
+    $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+    $row -$result-> fetch_assoc();
+
+    $correct_choice= $row['id'];
+
+    if($correct_choice == $selected_choice){
+        $_SESSION['score']++
+    }
+
+    if ($number == $total){
+        header("Location: final.php");
+        exit();
+    } else{
+        header("Location: quiz_question.php?q=1&n=".$next);
+    }
   }
 ?>
