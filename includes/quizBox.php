f2c556f3e3b1e54d99886de1ec5214b5d3b76acf
Semas2001 <semasarmonaitis@gmail.com>
2023-05-17 20:35:32 +0100

    Quiz box update made it so that it tells the user that they can only take the quiz once

--- includes/quizBox.php
+++ includes/quizBox.php
@@ -33,8 +33,10 @@
           $id =  $row['id'];
           echo "<div id='quiz$id' class='quiz' onclick='openquizbox()'>";
           echo"     <h1 class='quiz-header' id='Q$id'>$title</h1>
-                      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </div>
-                      <bk></bk>
+                      <div>Take this Quiz to get the score and progress with your course </div>
+                      <bk>
+                      <div>YOU CAN ONLY TAKE THIS QUIZ ONCE!!!</div>
+                      </bk>
                       <div class='progress-container'>";
           echo        displayUserScore($email, $id);
           echo        "</div>
